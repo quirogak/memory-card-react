0258128e7317cb81366403f32b72674d5b2dff40
quirogak <quirogakill@gmail.com>
2023-07-07 13:33:53 -0500

    Added logic to handle Deck randomizing. Used useEffect() to add event handlers on componentDidMount in each Card, so when a Card is clicked, the "randomize" state in Deck, is set to true.

--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,20 @@
-const Card = () => {
-    return (
-        <></>
-    )
-}
+import { useEffect } from "react";
+import "../styles/card.css";
+import uniqid from "uniqid";
 
-export default Card
+const Card = (props) => {
+  const currentId = uniqid();
+  const setRandomizeTrue = () => props.randomizeHandler(true);
+
+  useEffect(() => {
+    // call useEffect to put event listener on componentDidMount.
+    const currentCard = document.getElementById(currentId);
+    currentCard.addEventListener("click", setRandomizeTrue);
+
+    return () => currentCard.removeEventListener("click", setRandomizeTrue); // remove listener on componentWillUnmount.
+  }, []);
+
+  return <div className="card" id={currentId}></div>;
+};
+
+export default Card;
