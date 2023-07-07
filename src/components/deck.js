295f965eb81087cbfd251453d57f1b12f159b449
quirogak <quirogakill@gmail.com>
2023-07-07 13:32:46 -0500

    Organized logic to pass & trigger randomizeDeck.

--- src/components/deck.js
+++ src/components/deck.js
@@ -1,51 +1,64 @@
 import Card from "./card";
 import { useState } from "react";
 import uniqid from "uniqid";
+import "../styles/deck.css";
 
 const Deck = () => {
-    const deckArray = [
-        <Card key={uniqid()}></Card>,
-        <Card key={uniqid()}></Card>,
-        <Card key={uniqid()}></Card>,
-        <Card key={uniqid()}></Card>,
-        <Card key={uniqid()}></Card>,
-        <Card key={uniqid()}></Card>,
-        <Card key={uniqid()}></Card>,
-        <Card key={uniqid()}></Card>,
-        <Card key={uniqid()}></Card>,
-        <Card key={uniqid()}></Card>,
-        <Card key={uniqid()}></Card>,
-        <Card key={uniqid()}></Card>,
-    ];
+  const [randomize, setRandomize] = useState(false);
 
-    const randomizeArray = (arr) => {
-        const randomIndex = (max, min) => {
-            const randomNumber = Number.parseInt(Math.random() * (max - min) + min);
-            return randomNumber;
-        };
+  const deckArray = [
+    <Card key={uniqid()} randomizeHandler={setRandomize}></Card>,
+    <Card key={uniqid()} randomizeHandler={setRandomize}></Card>,
+    <Card key={uniqid()} randomizeHandler={setRandomize}></Card>,
+    <Card key={uniqid()} randomizeHandler={setRandomize}></Card>,
+    <Card key={uniqid()} randomizeHandler={setRandomize}></Card>,
+    <Card key={uniqid()} randomizeHandler={setRandomize}></Card>,
+    <Card key={uniqid()} randomizeHandler={setRandomize}></Card>,
+    <Card key={uniqid()} randomizeHandler={setRandomize}></Card>,
+    <Card key={uniqid()} randomizeHandler={setRandomize}></Card>,
+    <Card key={uniqid()} randomizeHandler={setRandomize}></Card>,
+    <Card key={uniqid()} randomizeHandler={setRandomize}></Card>,
+    <Card key={uniqid()} randomizeHandler={setRandomize}></Card>,
+  ];
 
-        const buildRandomArray = (arr) => {
-            const randomizedSet = new Set; // we need it to be a set, because when we push random card, it shouldn't be repeated.
+  const [currentDeck, setCurrentDeck] = useState(deckArray);
 
-            const fillSet = () => {
-                // on this algorithm, there is no need for comparing if the value already exists, because of the definition of Set.
-                // when the randomElement is already inside of randomizedSet, the Set won't increase size,
-                // so the recursive call will keep running until we have a full randomized Set with different cards, triggering the base case.
-                if (randomizedSet.size === arr.length) return randomizedSet;
-                const randomNumber = randomIndex(arr.length, 0);
-                const randomElement = arr[randomNumber];
-                randomizedSet.add(randomElement);
-                return fillSet();
-            };
-
-            return fillSet(); // first call to recursive function
-        };
-        return buildRandomArray(arr);
+  const randomizeArray = (arr) => {
+    const randomIndex = (max, min) => {
+      const randomNumber = Number.parseInt(Math.random() * (max - min) + min);
+      return randomNumber;
     };
 
-    const randomizedDeck = Array.from(randomizeArray(deckArray));
+    const buildRandomArray = (arr) => {
+      const randomizedSet = new Set(); // we need it to be a set, because when we push random card, it shouldn't be repeated.
 
-    return <div id="Deck">{deckArray.map((card) => card)}</div>;
+      const fillSet = () => {
+        // on this algorithm, there is no need for comparing if the value already exists, because of the definition of Set.
+        // when the randomElement is already inside of randomizedSet, the Set won't increase size,
+        // so the recursive call will keep running until we have a full randomized Set with different cards, triggering the base case.
+        if (randomizedSet.size === arr.length) return randomizedSet;
+        const randomNumber = randomIndex(arr.length, 0);
+        const randomElement = arr[randomNumber];
+        randomizedSet.add(randomElement);
+        return fillSet();
+      };
+
+      return fillSet(); // first call to recursive function
+    };
+    return buildRandomArray(arr);
+  };
+
+  const randomizeDeck = () => {
+    setCurrentDeck(Array.from(randomizeArray(deckArray))); // set currentDeck to a randomized deck.
+    return setRandomize(false); // so this function is called once at a time.
+  };
+
+  return (
+    <div id="Deck">
+      {randomize ? randomizeDeck() : null}
+      {currentDeck.map((card) => card)}
+    </div>
+  );
 };
 
 export default Deck;
