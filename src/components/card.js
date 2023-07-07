cf57e536c8455986e680663422b3877e38b13e91
quirogak <quirogakill@gmail.com>
2023-07-07 16:26:09 -0500

    Modified Card JSX returned.

--- src/components/card.js
+++ src/components/card.js
@@ -3,18 +3,25 @@ import "../styles/card.css";
 import uniqid from "uniqid";
 
 const Card = (props) => {
-  const currentId = uniqid();
-  const setRandomizeTrue = () => props.randomizeHandler(true);
+    const currentId = uniqid();
+    const setRandomizeTrue = () => props.randomizeHandler(true);
 
-  useEffect(() => {
-    // call useEffect to put event listener on componentDidMount.
-    const currentCard = document.getElementById(currentId);
-    currentCard.addEventListener("click", setRandomizeTrue);
+    useEffect(() => {
+        // call useEffect to put event listener on componentDidMount.
+        const currentCard = document.getElementById(currentId);
+        currentCard.addEventListener("click", setRandomizeTrue);
 
-    return () => currentCard.removeEventListener("click", setRandomizeTrue); // remove listener on componentWillUnmount.
-  }, []);
+        return () => currentCard.removeEventListener("click", setRandomizeTrue); // remove listener on componentWillUnmount.
+    }, []);
 
-  return <div className="card" id={currentId}></div>;
+    return (
+        <div className="card" id={currentId}>
+            <figure>
+                <img src={props.imgSrc} alt={props.name}></img>
+                <figcaption>{props.name}</figcaption>
+            </figure>
+        </div>
+    )
 };
 
 export default Card;
