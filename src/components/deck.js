e35ac606d72bf300135de5c1cfa5373577d39a44
quirogak <quirogakill@gmail.com>
2023-07-09 14:18:52 -0500

    Cleaned Components props + added scoreboard states

--- src/components/deck.js
+++ src/components/deck.js
@@ -2,25 +2,43 @@ import Card from "./card";
 import { useState } from "react";
 import uniqid from "uniqid";
 import "../styles/deck.css";
+import Scoreboard from "./scoreboard";
 
-
+const CardsInfo = [
+    { listId: 0, name: "Aragorn", imgSrc: require("../img/aragorn.jpg") },
+    { listId: 1, name: "Legolas", imgSrc: require("../img/legolas.jpg") },
+    { listId: 2, name: "Bilbo", imgSrc: require("../img/bilbo.jpg") },
+    { listId: 3, name: "Frodo", imgSrc: require("../img/frodo.jpg") },
+    { listId: 4, name: "Gandalf", imgSrc: require("../img/gandalf.jpg") },
+    { listId: 5, name: "Gollum", imgSrc: require("../img/gollum.jpg") },
+    { listId: 6, name: "Samsagaz", imgSrc: require("../img/samsagaz.jpg") },
+    { listId: 7, name: "Gimli", imgSrc: require("../img/gimli.jpg") },
+    { listId: 8, name: "Sauron", imgSrc: require("../img/sauron.jpg") },
+    { listId: 9, name: "Thorin", imgSrc: require("../img/thorin.jpg") },
+    { listId: 10, name: "Saruman", imgSrc: require("../img/saruman.jpg") },
+    { listId: 11, name: "Galadriel", imgSrc: require("../img/galadriel.jpg") },
+]
 
 const Deck = () => {
     const [randomize, setRandomize] = useState(false);
+    const [score, setScore] = useState(0)
+    const [bestScore, setBestScore] = useState(0)
+    const [clicked, setClicked] = useState(Array(12).fill(false)) // Array with 12 repeated false values.
+    const stateProps = { randomizeHandler: setRandomize, score: score, setScore: setScore, clicked: clicked, setClicked: setClicked }
 
     const deckArray = [
-        <Card key={uniqid()} randomizeHandler={setRandomize} name="Aragorn" imgSrc={require("../img/aragorn.jpg")}></Card>,
-        <Card key={uniqid()} randomizeHandler={setRandomize} name="Legolas" imgSrc={require("../img/legolas.jpg")}></Card>,
-        <Card key={uniqid()} randomizeHandler={setRandomize} name="Bilbo Baggins" imgSrc={require("../img/bilbo.jpg")}></Card>,
-        <Card key={uniqid()} randomizeHandler={setRandomize} name="Frodo Baggins" imgSrc={require("../img/frodo.jpg")}></Card>,
-        <Card key={uniqid()} randomizeHandler={setRandomize} name="Gandalf" imgSrc={require("../img/gandalf.jpg")}></Card>,
-        <Card key={uniqid()} randomizeHandler={setRandomize} name="Gollum" imgSrc={require("../img/gollum.jpg")}></Card>,
-        <Card key={uniqid()} randomizeHandler={setRandomize} name="Samsagaz" imgSrc={require("../img/samsagaz.jpg")}></Card>,
-        <Card key={uniqid()} randomizeHandler={setRandomize} name="Gimli" imgSrc={require("../img/gimli.jpg")}></Card>,
-        <Card key={uniqid()} randomizeHandler={setRandomize} name="Sauron" imgSrc={require("../img/sauron.jpg")}></Card>,
-        <Card key={uniqid()} randomizeHandler={setRandomize} name="Thorin" imgSrc={require("../img/thorin.jpg")}></Card>,
-        <Card key={uniqid()} randomizeHandler={setRandomize} name="Saruman" imgSrc={require("../img/saruman.jpg")}></Card>,
-        <Card key={uniqid()} randomizeHandler={setRandomize} name="Galadriel" imgSrc={require("../img/galadriel.jpg")}></Card>,
+        <Card key={uniqid()} cardInfo={CardsInfo[0]} {...stateProps}></Card>,
+        <Card key={uniqid()} cardInfo={CardsInfo[1]} {...stateProps}></Card>,
+        <Card key={uniqid()} cardInfo={CardsInfo[2]} {...stateProps}></Card>,
+        <Card key={uniqid()} cardInfo={CardsInfo[3]} {...stateProps}></Card>,
+        <Card key={uniqid()} cardInfo={CardsInfo[4]} {...stateProps}></Card>,
+        <Card key={uniqid()} cardInfo={CardsInfo[5]} {...stateProps}></Card>,
+        <Card key={uniqid()} cardInfo={CardsInfo[6]} {...stateProps}></Card>,
+        <Card key={uniqid()} cardInfo={CardsInfo[7]} {...stateProps}></Card>,
+        <Card key={uniqid()} cardInfo={CardsInfo[8]} {...stateProps}></Card>,
+        <Card key={uniqid()} cardInfo={CardsInfo[9]} {...stateProps}></Card>,
+        <Card key={uniqid()} cardInfo={CardsInfo[10]} {...stateProps}></Card>,
+        <Card key={uniqid()} cardInfo={CardsInfo[11]} {...stateProps}></Card>,
     ];
 
     const [currentDeck, setCurrentDeck] = useState(deckArray);
@@ -56,10 +74,13 @@ const Deck = () => {
     };
 
     return (
-        <div id="Deck">
-            {randomize ? randomizeDeck() : null}
-            {currentDeck.map((card) => card)}
-        </div>
+        <>
+            <div id="Deck">
+                {randomize ? randomizeDeck() : null}
+                {currentDeck.map((card) => card)}
+            </div>
+            <Scoreboard score={score} bestScore={bestScore}></Scoreboard>
+        </>
     );
 };
 
