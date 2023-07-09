b4791b44b881bd22646eddeefc87694e0ca4da23
quirogak <quirogakill@gmail.com>
2023-07-09 14:21:14 -0500

    Added addScore logic on card click. Also builded the logic to avoid addScore when the card was already clicked.

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,21 @@
 import { useEffect } from "react";
 import "../styles/card.css";
-import uniqid from "uniqid";
 
 const Card = (props) => {
-    const currentId = uniqid();
+    const currentId = props.cardInfo.listId;
     const setRandomizeTrue = () => props.randomizeHandler(true);
 
+    const addScore = () => {
+        if (!props.clicked[currentId]) props.setScore(props.score + 1)
+        const mutableClicks = props.clicked  // create a copy of "clicked array" prop.
+        mutableClicks[currentId] = true // set the "clicked value" of this card as true.
+        props.setClicked(mutableClicks) // then, setClicked as the updated array.
+    }
+
     useEffect(() => {
-        // call useEffect to put event listener on componentDidMount.
+        // call useEffect to add event handlers on componentDidMount.
         const currentCard = document.getElementById(currentId);
+        currentCard.addEventListener("click", addScore)
         currentCard.addEventListener("click", setRandomizeTrue);
 
         return () => currentCard.removeEventListener("click", setRandomizeTrue); // remove listener on componentWillUnmount.
@@ -17,8 +24,8 @@ const Card = (props) => {
     return (
         <div className="card" id={currentId}>
             <figure>
-                <img src={props.imgSrc} alt={props.name}></img>
-                <figcaption>{props.name}</figcaption>
+                <img src={props.cardInfo.imgSrc} alt={props.cardInfo.name}></img>
+                <figcaption>{props.cardInfo.name}</figcaption>
             </figure>
         </div>
     )
