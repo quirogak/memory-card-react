6583344bf5df17ab424d91d33658606a87d7e198
quirogak <quirogakill@gmail.com>
2023-07-10 12:33:00 -0500

    Added best score logic + reset round.

--- src/components/card.js
+++ src/components/card.js
@@ -2,33 +2,54 @@ import { useEffect } from "react";
 import "../styles/card.css";
 
 const Card = (props) => {
-    const currentId = props.cardInfo.listId;
-    const setRandomizeTrue = () => props.randomizeHandler(true);
+  const currentId = props.cardInfo.listId;
+  const setRandomizeTrue = () => props.randomizeHandler(true);
 
-    const addScore = () => {
-        if (!props.clicked[currentId]) props.setScore(props.score + 1)
-        const mutableClicks = props.clicked  // create a copy of "clicked array" prop.
-        mutableClicks[currentId] = true // set the "clicked value" of this card as true.
-        props.setClicked(mutableClicks) // then, setClicked as the updated array.
-    }
+  const updateBestScore = (newScore) => props.setBestScore(newScore);
 
-    useEffect(() => {
-        // call useEffect to add event handlers on componentDidMount.
-        const currentCard = document.getElementById(currentId);
-        currentCard.addEventListener("click", addScore)
-        currentCard.addEventListener("click", setRandomizeTrue);
+  const resetClickedCards = () =>
+    props.setClicked(Array(props.clicked.length).fill(false)); // set every "clicked" cards element to false.
 
-        return () => currentCard.removeEventListener("click", setRandomizeTrue); // remove listener on componentWillUnmount.
-    }, []);
+  const addScore = () => {
+    props.setScore(props.score + 1);
+    const mutableClicks = props.clicked; // create a copy of "clicked array" prop.
+    mutableClicks[currentId] = true; // set the "clicked value" of this card as true.
+    props.setClicked(mutableClicks); // then, setClicked as the updated array.
+  };
 
-    return (
-        <div className="card" id={currentId}>
-            <figure>
-                <img src={props.cardInfo.imgSrc} alt={props.cardInfo.name}></img>
-                <figcaption>{props.cardInfo.name}</figcaption>
-            </figure>
-        </div>
-    )
+  const resetRound = () => {
+    if (props.score >= props.bestScore) updateBestScore(props.score);
+    resetClickedCards();
+    props.setScore(0);
+  };
+
+  const updateScoreboard = () => {
+    if (props.clicked[currentId])
+      resetRound(); // if the current card is already clicked, reset the round.
+    else addScore();
+  };
+
+  useEffect(() => {
+    // call useEffect to add event handlers on componentDidMount.
+    const currentCard = document.getElementById(currentId);
+    currentCard.addEventListener("click", updateScoreboard);
+    currentCard.addEventListener("click", setRandomizeTrue);
+
+    return () => {
+      // remove listeners on componentWillUnmount.
+      currentCard.removeEventListener("click", setRandomizeTrue);
+      currentCard.removeEventListener("click", updateScoreboard);
+    };
+  }, []);
+
+  return (
+    <div className="card" id={currentId}>
+      <figure>
+        <img src={props.cardInfo.imgSrc} alt={props.cardInfo.name}></img>
+        <figcaption>{props.cardInfo.name}</figcaption>
+      </figure>
+    </div>
+  );
 };
 
 export default Card;
