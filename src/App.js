cce01c30c1a1fe7d079fba6fe244821330fb32f3
quirogak <quirogakill@gmail.com>
2023-07-10 12:41:52 -0500

    Organized structure + removed unnecessary modules.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import Header from "./components/header.js";
 import Footer from "./components/footer.js";
 import Deck from "./components/deck";
-import Scoreboard from "./components/scoreboard.js";
 import "./styles/app.css"
 
 function App() {
   return (
     <div className="App">
       <Header title="The Lord Of The Rings" subtitle="Memory Game"></Header>
-      <body>
+      <section id="body">
         <Deck></Deck>
-        <Scoreboard></Scoreboard>
-      </body>
+      </section>
       <Footer content="@quirogak" href="https://github.com/quirogak"></Footer>
     </div>
   );
